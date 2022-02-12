9e22e7b229a81e2aee24068717d1ef71a1162e21
Nayara Gomes <nayara.gomes13@poli.ufrj.br>
2022-02-12 03:55:20 -0300

    Finalizando header user info da home

--- frontend/src/app/layout/home/home.component.ts
+++ frontend/src/app/layout/home/home.component.ts
@@ -7,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  public user = {
+    name: 'Nayara',
+    email: 'nayara.gomes@g.globo',
+  }
+
   constructor() { }
 
   ngOnInit(): void {
